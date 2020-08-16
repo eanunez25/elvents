988cb3f454bbedea7b83d6dca306748b0bec02ac
Elvin Nunez <eanunez25@gmail.com>
2020-08-16 00:08:55 -0500

    create user show and add create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Elvents!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username)
+    end
 
 end
